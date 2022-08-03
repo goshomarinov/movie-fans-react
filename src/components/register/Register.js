621fddaa7e187b69ffa97b5682382f1ad252c1b5
goshomarinov <georgimarinov1990@abv.bg>
2022-08-03 07:04:33 +0300

    Added controlled register form and validators

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,18 +1,103 @@
 import registerStyles from '../register/Register.module.css';
 
+import { useState } from 'react';
+import {
+    repassValidator,
+    usernameValidator,
+    passwordValidator,
+    registerBtnValidator
+} from '../../utils/validators';
+
 export const Register = () => {
-    return(
+    const [errors, setErrors] = useState({
+        userErr: '',
+        passErr: '',
+        rePassErr: '',
+    })
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+        rePassword: '',
+    });
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const errorCheck = (e) => {
+        if (e.target.name == 'username') {
+            setErrors(errors => ({
+                ...errors,
+                userErr: usernameValidator(values.username),
+            }))
+        } else if (e.target.name == 'password') {
+            setErrors(errors => ({
+                ...errors,
+                passErr: passwordValidator(values.password),
+            }))
+        } else if (e.target.name == 'rePassword') {
+            setErrors(errors => ({
+                ...errors,
+                rePassErr: repassValidator(values),
+            }))
+        }
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(Object.fromEntries(new FormData(e.target)))
+    };
+
+    return (
         <section className={registerStyles['register']}>
-        <img src="https://st.depositphotos.com/2036511/3067/v/450/depositphotos_30675099-stock-illustration-round-register-now-button.jpg" alt=""/>
-        <form className={registerStyles['register-form']}>
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" value="" placeholder="Username"/>
-            <label for="password">Password</label>
-            <input type="text" id="password" name="password" value="" placeholder="Password"/>
-            <label for="rePassword">Password</label>
-            <input type="text" id="rePassword" name="rePassword" value="" placeholder="Repeat Password"/>
-            <input className={registerStyles['register-btn']} type="submit" value="Register"/>
-        </form>
-    </section>
+            <img src="https://st.depositphotos.com/2036511/3067/v/450/depositphotos_30675099-stock-illustration-round-register-now-button.jpg" alt="" />
+            <form className={registerStyles['register-form']} onSubmit={onSubmit}>
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    onChange={changeHandler}
+                    value={values.username}
+                    onBlur={errorCheck}
+                    placeholder="Username"
+                />
+                {errors.userErr}
+
+                <label htmlFor="password">Password</label>
+                <input
+                    type="text"
+                    id="password"
+                    name="password"
+                    onChange={changeHandler}
+                    value={values.password}
+                    onBlur={errorCheck}
+                    placeholder="Password"
+                />
+                {errors.passErr}
+
+                <label htmlFor="rePassword">Repeat Password</label>
+                <input
+                    type="text"
+                    id="rePassword"
+                    name="rePassword"
+                    onChange={changeHandler}
+                    value={values.rePassword}
+                    onBlur={errorCheck}
+                    placeholder="Repeat Password"
+                />
+                {errors.rePassErr}
+
+                <input
+                    className={registerStyles['register-btn']}
+                    type="submit"
+                    value="Register"
+                    disabled={registerBtnValidator(values)}
+                />
+            </form>
+        </section>
     );
 };
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,6 +1,6 @@
 export const usernameValidator = (username) => {
     if (username.length < 4) {
-        return(
+        return (
             <span style={{ color: 'red' }}>Username must be minimum 4 characters long!</span>
         );
     } else {
@@ -10,7 +10,7 @@ export const usernameValidator = (username) => {
 
 export const passwordValidator = (password) => {
     if (password.length < 6) {
-        return(
+        return (
             <span style={{ color: 'red' }}>Password must be minimum 6 characters long!</span>
         );
     } else {
@@ -18,8 +18,32 @@ export const passwordValidator = (password) => {
     }
 }
 
+export const repassValidator = (values) => {
+    if (values.rePassword.length < 6) {
+        return (
+            <span style={{ color: 'red' }}>Repeat password must be minimum 6 characters long!</span>
+        );
+    } else if (values.rePassword != values.password) {
+        return (
+            <span style={{ color: 'red' }}>Passwords don't match</span>
+        );
+    } else {
+        return '';
+    }
+}
 export const loginBtnValidator = (values) => {
-    if(values.username.length < 4 || values.password.length < 6) {
+    if (values.username.length < 4 || values.password.length < 6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const registerBtnValidator = (values) => {
+    if (values.username.length < 4
+        || values.password.length < 6
+        || values.rePassword.length < 6
+        || values.rePassword != values.password) {
         return true;
     } else {
         return false;
